99ac8801cb58c95204906fd1bac5c9ba2f994e97
bhaveshvasita10-netizen <bhaveshvasita10@gmail.com>
2026-09-17 00:02:39 +0530

    Build full Brainrot Studio dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import CharacterStudio from '@/components/CharacterStudio';
+import BrainrotStudio from '@/components/BrainrotStudio';
 
 export const dynamic = 'force-dynamic';
 
-export default function Home(){ return <CharacterStudio />; }
+export default function Home() {
+  return <BrainrotStudio />;
+}
